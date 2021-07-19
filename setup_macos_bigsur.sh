7c0834fbaaf4a50d91e81e87e3a03bbfcef27ad3
Michael J. Booth <mjboothaus@gmail.com>
2021-07-19 16:07:18 +1000

    Update setup_macos_bigsur.sh

--- setup_macos_bigsur.sh
+++ setup_macos_bigsur.sh
@@ -1,5 +1,7 @@
 # Capture the setup from my 12" MacBook (early 2015) - clean Big Sur install (11.4)
 
+## iCloud **** make sure Photos are turned off **** use for Notes, KeyChain
+
 # c.f. https://github.com/herrbischoff/awesome-macos-command-line  - lots of info here
 
 # Display(s)
